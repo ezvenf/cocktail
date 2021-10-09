1c77678c82405389b4f9fb6b2afdb5103adb567d
ezvenf <ezvenf@gmail.com>
2021-10-09 21:08:23 +0530

    Reorganise

--- src/App.js
+++ src/App.js
@@ -13,7 +13,9 @@ const App = () => {
       <div className="search">
         <Link to="/"><h1>Cocktail</h1></Link>
         <Switch>
-          <Route exact path="/" component={SearchField} />
+          <Route exact path="/">
+            <SearchField />
+          </Route>
           <Route path="*">
             <h1 className="text-center">404</h1>
           </Route>
